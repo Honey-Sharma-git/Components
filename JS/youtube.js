83a5c0fb4aeb378411f8b734b8933ff1d5b49cc0
Honey-Sharma-git <Honeysharmaemail@gmail.com>
2024-11-27 17:44:36 +0530

    Add event listeners

--- JS/youtube.js
+++ JS/youtube.js
@@ -1,12 +1,21 @@
-function subscribed(){
-    let buttonStatus = document.querySelector('.js-subscribe-button');
-    
-    if(buttonStatus.innerText === 'Subscribe'){
-        buttonStatus.innerHTML = 'Subscribed';
-        document.querySelector('.js-subscribe-button').classList.add('subscribed-button');
-    } else {
-        buttonStatus.innerHTML = 'Subscribe';
-    document.querySelector('.js-subscribe-button').classList.remove('subscribed-button');
-    }
+//SELECTING ELEMENTS
+const buttonElement = document.querySelector(".js-subscribe-button");
+//ADDING EVENT LISTENERS
+buttonElement.addEventListener("click", () => {
+  subscribed();
+});
+function subscribed() {
+  let buttonStatus = document.querySelector(".js-subscribe-button");
 
+  if (buttonStatus.innerText === "Subscribe") {
+    buttonStatus.innerHTML = "Subscribed";
+    document
+      .querySelector(".js-subscribe-button")
+      .classList.add("subscribed-button");
+  } else {
+    buttonStatus.innerHTML = "Subscribe";
+    document
+      .querySelector(".js-subscribe-button")
+      .classList.remove("subscribed-button");
+  }
 }
